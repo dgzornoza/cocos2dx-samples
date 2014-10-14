2bf50bc1c34ef0b64cf4f385eeb376ee49175456
David Gonzalez <zelion_spn@hotmail.com>
2014-10-14 23:48:14 +0200

    version alfa zplugin

---
+++ Source/proj.wp8-xaml/App/ZPlugins/BaseCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneDirect3DXamlAppInterop.ZPlugins
+{
+    /// <summary>
+    /// Base class for create plugins
+    /// </summary>
+    public abstract class BaseCommand
+    {
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public BaseCommand()
+        {
+        }
+
+        /// <summary>callback for success result</summary>
+        public PhoneDirect3DXamlAppComponent.CompletedFunc SuccessCallback { get; set; }
+
+        /// <summary>callback for error result</summary>
+        public PhoneDirect3DXamlAppComponent.CompletedFunc ErrorCallback { get; set; }
+
+        /// <summary>
+        /// Function for dispatch default plugin command result
+        /// </summary>
+        public void DispatchCommandResult()
+        {
+            this.DispatchCommandResult(new PluginResult(PluginResult.Status.NO_RESULT));
+        }
+
+        /// <summary>
+        /// Function for dispatch custom plugin command result
+        /// </summary>
+        /// <param name="result">Plugin result object</param>
+        public void DispatchCommandResult(PluginResult _result)
+        {
+            // invoke callback
+            if (_result.StatusResult == PluginResult.Status.OK || _result.StatusResult == PluginResult.Status.NO_RESULT)
+            {
+                SuccessCallback.Invoke(_result.ToJSONString());
+            }
+            else SuccessCallback.Invoke(_result.ToJSONString());
+        }
+    }
+}
